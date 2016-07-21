3756031c11742d79605d575a3362631878472d92
Apprentice <apprentice@StMac0-6.local>
2016-07-22 11:00:05 +1200

    adding routes

---
+++ routes/login.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var passport = require('passport')
+var router = express.Router()
+
+router.post(
+  '/',
+  passport.authenticate('local', {
+    successRedirect: '/lose',
+    
+  })
+)
