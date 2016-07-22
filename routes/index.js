dd59d4d50f073e9a611b03f15dadfc0e59039ace
rich-farr <scarletmanuka@gmail.com>
2016-07-22 14:21:31 +1200

    Complete styling on login page

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', ensureAuthenticated, function(req, res, next) {
-  res.render('/index', { title: 'Express' });
+router.get('/', /*ensureAuthenticated,*/ function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-function ensureAuthenticated(req, res, next){
-  if (req.isAuthenticated()){return next()}
-  res.redirect('login')
-}
+// function ensureAuthenticated(req, res, next){
+//   if (req.isAuthenticated()){return next()}
+//   res.redirect('login')
+// }
 module.exports = router;
-
